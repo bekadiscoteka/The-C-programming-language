614ebab885b796117166b76a0822c35e3e55d9ed
littlebrother <yermanbekzat@gmail.com>
2026-04-17 19:31:36 +0500

    Add file comparision

--- chapter-7/fcmp/fcmp.c
+++ chapter-7/fcmp/fcmp.c
@@ -1,5 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAXLINE 100
+// fopen with error printing
 
-int main(void) {
+FILE* fopen_err(char* name, char* mode) {
+	FILE* ftemp = fopen(name, mode);
+	if (ftemp == NULL) {
+		exit(2);
+		fprintf(stderr, "Cannot open file %s, make sure file exists and you have appropriate permissions\n", name);
+	}
+	return ftemp;	
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
+		fprintf(stderr, "There should be 2 file names\n");
+		exit(1);
+	}
 	
+	FILE *file_a = fopen_err(argv[1], "r");
+	FILE *file_b = fopen_err(argv[2], "r");
+
+	char line_a[MAXLINE], line_b[MAXLINE];
+	char *strtempa, *strtempb;
+	int line = -1;
+
+	do {
+		line++;
+		strtempa = fgets(line_a, MAXLINE, file_a); 
+		strtempb = fgets(line_b, MAXLINE, file_b);
+		if (strtempb == NULL && strtempa == NULL) {
+			printf("Files are equal!\n");
+			return 0;
+		}
+		else if (strtempb == NULL) {
+			printf("%s is subset of %s\n", argv[2], argv[1]);
+			return 0;
+		}
+		else if (strtempa == NULL) {
+			printf("%s is subset of %s\n", argv[1], argv[2]);
+			return 0;
+		}
+
+	} while (strcmp(strtempa, strtempb) == 0);
+	
+	fclose(file_a); fclose(file_b);
+
+	printf("Files differ in line #%d:\n%s: %s%s: %s", line, argv[1], strtempa, argv[2], strtempb);
+	
+	return 0;
 }
