3d690bfdcdde357ed8170db178f90b3a2669fa4d
littlebrother <yermanbekzat@gmail.com>
2026-04-20 12:02:09 +0500

    buffer fix

--- chapter-8/minicat/minicat.c
+++ chapter-8/minicat/minicat.c
@@ -2,7 +2,8 @@
 #include <syscall.h>
 #include <fcntl.h>
 #include <string.h>
-#define BUFSIZE 10
+#include <stddef.h>
+#define BUFSIZE 100
 
 int main(int argc, char* argv[]) {
 	if (argc != 2) {
@@ -11,15 +12,17 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 	int fd;
-	char buf;
+	size_t wr_bufsize;
+	char buf[BUFSIZE];
+
 	if ( (fd = open(*++argv, O_RDONLY, 0)) == -1) {
 		char errmsg[100] = "Usage: cat filename\n";
 		sprintf(errmsg, "Can't open file %s, with %3o permission\n", *argv, O_RDONLY);
 		write( 2, errmsg, (sizeof(errmsg[1]) * (strlen(errmsg)+1)) );
 		return 1;
 	}
-	
-	while ((read(fd, &buf, 1)) > 0) 
-		write(1, &buf, 1);	
+
+	while ((wr_bufsize=read(fd, buf, BUFSIZE)) > 0)
+		write(1, buf, wr_bufsize);	
 	return 0;
 }
